454911c1d38f182977162fe9ae8fce0a9f3e9437
chiuzon <chiuzon@protonmail.com>
2022-04-08 13:39:20 +0300

    Made with alpine js

---
+++ app.js
@@ -0,0 +1,13 @@
+const APP_CONSTANTS = {
+  GITHUB_LINK: "https://github.com/chiuzon",
+  TWITTER_LINK: "https://twitter.com/chiuzon",
+  OSS_PROJECTS: [
+    {
+      title: "svelteWeb3",
+      description:
+        "simple, dependency minimized package for building dApps with Svelte",
+      href: "https://github.com/chiuzon/svelteWeb3",
+    },
+  ],
+  PUBLICATIONS: [],
+};
